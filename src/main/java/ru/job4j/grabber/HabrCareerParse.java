63e34f0316b1f4ba0782996a1ab6d3c9bad2cc37
s.starikov <s.starikov@gaskar.group>
2024-08-10 00:58:14 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -4,11 +4,11 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class HabrCareerParse {
@@ -30,10 +30,9 @@ public class HabrCareerParse {
             Elements rows = document.select(".vacancy-card__inner");
             elements.add(rows);
         }
-        elements.stream()
-            .peek(rows -> System.out.println("Part records: " + rows.size()))
-            .flatMap(Collection::stream)
-            .forEach(row -> {
+        for (Elements rows : elements) {
+            System.out.println("Part records: " + rows.size());
+            for (Element row : rows) {
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element dateElement = row.select(".vacancy-card__date").first();
                 Element linkElement = titleElement.child(0);
@@ -41,6 +40,17 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 System.out.printf("date: [%s]; vacancy name: [%s]; link: [%s]%n", date, vacancyName, link);
-            });
+                String string = retrieveDescription(link);
+                System.out.printf("Description, %s%n%n", string);
+            }
+        }
+    }
+
+    private static String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-description__text");
+        Element element = rows.get(0);
+        return element.text();
     }
 }
