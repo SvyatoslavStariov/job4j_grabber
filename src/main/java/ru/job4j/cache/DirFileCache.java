5ef81bf8d0b48d7b5b86c2371ee67adb923fa7a7
s.starikov <s.starikov@gaskar.group>
2024-12-04 22:28:14 +0500

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,23 @@
+package ru.job4j.cache;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key)  {
+        try {
+            return Files.readString(Paths.get(cachingDir));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
